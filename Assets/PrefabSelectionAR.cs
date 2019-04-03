8faa244f35a7cdbdc6cc3130b2f675318d6484e6
c-tsalidis <ctsalidis1999@gmail.com>
2019-04-03 14:47:25 +0200

    Added crosshair functionality

--- Assets/PrefabSelectionAR.cs
+++ Assets/PrefabSelectionAR.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class PrefabSelectionAR : MonoBehaviour
 {
+
+    // This script is based on this tutorial:
+    // https://www.youtube.com/watch?v=hi_KDpC1nzk&t=317s
+
     private string prefabName;
 
     public Text text;
@@ -18,6 +22,7 @@ public class PrefabSelectionAR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        text.text = "Point at a cube";
         // if it's a phone
         if((Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began))
         {
@@ -32,6 +37,16 @@ public class PrefabSelectionAR : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             selectPrefab(ray);
         }
+        // if the user is selecting it with the camera's view
+        else
+        {
+            // how to send a ray from the camera's center:
+            // https://docs.unity3d.com/ScriptReference/Camera.ViewportPointToRay.html
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0)); // 0.5f in x and 0.5f in y in the center of the screen (goes from (0,0) to (1,1))
+            selectPrefab(ray);
+        }
+        
+        
     }
 
     private void selectPrefab(Ray ray)
@@ -50,7 +65,7 @@ public class PrefabSelectionAR : MonoBehaviour
                             Debug.Log(prefabName + " Touched");
                             text.text = prefabName + " Touched";
                             break;
-                default:
+                default:    text.text = "Point at \n a cube";
                         break;
             }
         }
