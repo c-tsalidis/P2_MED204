b1451232f24292835c3078c3b62b54054dbab24f
c-tsalidis <ctsalidis1999@gmail.com>
2019-04-04 17:03:57 +0200

    Added the different parts of the boat and its' selection
    Now the different parts of the boat can be selected and it'll take to its own scene

--- Assets/PrefabSelectionAR.cs
+++ Assets/PrefabSelectionAR.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PrefabSelectionAR : MonoBehaviour
 {
@@ -10,8 +11,17 @@ public class PrefabSelectionAR : MonoBehaviour
     // https://www.youtube.com/watch?v=hi_KDpC1nzk&t=317s
 
     private string prefabName;
+    private string statusOfText = "Point at a part";
 
     public Text text;
+
+    public GameObject [] prefabs;
+    public Material [] materials;
+
+    public bool [] isSelected;
+
+    private Manager managerObject;
+    public string [] sceneLoadStrings;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +32,14 @@ public class PrefabSelectionAR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = "Point at a cube";
+        text.text = statusOfText;
+
+        for(int i = 0; i < prefabs.Length; i++)
+        {
+            changeMaterial(i, 0);
+            // isSelected[i] = false;
+        }
+
         // if it's a phone
         if((Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began))
         {
@@ -45,8 +62,7 @@ public class PrefabSelectionAR : MonoBehaviour
             Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0)); // 0.5f in x and 0.5f in y in the center of the screen (goes from (0,0) to (1,1))
             selectPrefab(ray);
         }
-        
-        
+             
     }
 
     private void selectPrefab(Ray ray)
@@ -55,18 +71,37 @@ public class PrefabSelectionAR : MonoBehaviour
         if(Physics.Raycast(ray, out hit))
         {
             prefabName = hit.transform.name;
-            switch(prefabName)
+            for(int i = 0; i < prefabs.Length; i++)
             {
-                case "Green Cube":
-                            Debug.Log(prefabName + " Touched");
-                            text.text = prefabName + " Touched";
-                            break;
-                case "Red Cube":
-                            Debug.Log(prefabName + " Touched");
-                            text.text = prefabName + " Touched";
-                            break;
-                default:    text.text = "Point at \n a cube";
-                        break;
+                if(prefabName == prefabs[i].name)
+                {
+                    Debug.Log("Found " + prefabName + " which is " + prefabs[i].name);
+                    changeMaterial(i, 1);
+                    isSelected[i] = true;
+                    text.text = prefabName;
+                }
+                else
+                {
+                    changeMaterial(i, 0);
+                    isSelected[i] = false;
+                }
+            }
+        }
+    }
+
+    private void changeMaterial(int prefabNumber, int materialNumber)
+    {
+        prefabs[prefabNumber].GetComponent<Renderer>().material = materials[materialNumber];
+    }
+
+    public void selectButton()
+    {
+        for(int i = 0; i < prefabs.Length; i++)
+        {
+            if(isSelected[i] == true)
+            {
+                // managerObject.LoadScene(sceneLoadStrings[i]);
+                SceneManager.LoadScene(sceneLoadStrings[i]);
             }
         }
     }
